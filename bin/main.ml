e4ccce151973a349133364eff7f2f14bb7eab650
Vladimir Kidyaev <krios2146@gmail.com>
2024-10-19 19:57:38 +0700

    feat: add shitty corrupting & decoding

--- bin/main.ml
+++ bin/main.ml
@@ -2,7 +2,7 @@
 
 (* Always 11 bits *)
 let data = [ 1; 0; 1; 1; 0; 1; 0; 0; 0; 0; 1 ]
-(* let flip x = match x with 0 -> 1 | 1 -> 0 | _ -> 0 *)
+let flip x = match x with 0 -> 1 | 1 -> 0 | _ -> 0
 
 let update_list_at (list : 'a list) i (v : 'a) =
   let rec aux list i acc =
@@ -120,4 +120,100 @@ let encode data =
   let all_sum = List.fold_left Int.add 0 data in
   if not (is_even all_sum) then update_list_at data 0 1 else data
 
-let () = encode data |> pretty_print
+let calculate_error_bit_pos first_parity_bit_sum second_parity_bit_sum =
+  match (is_even first_parity_bit_sum, is_even second_parity_bit_sum) with
+  | false, false -> 4
+  | false, true -> 2
+  | true, false -> 3
+  | true, true -> 1
+
+let decode data =
+  let second_col = get_nth_col data 2 in
+  let third_col = get_nth_col data 3 in
+  let forth_col = get_nth_col data 4 in
+
+  let second_forth_col_sum =
+    List.fold_left2 (fun acc a b -> acc + a + b) 0 second_col forth_col
+  in
+  let third_forth_col_sum =
+    List.fold_left2 (fun acc a b -> acc + a + b) 0 third_col forth_col
+  in
+
+  let second_row = get_nth_row data 2 in
+  let third_row = get_nth_row data 3 in
+  let forth_row = get_nth_row data 4 in
+
+  let second_forth_row_sum =
+    List.fold_left2 (fun acc a b -> acc + a + b) 0 second_row forth_row
+  in
+  let third_forth_row_sum =
+    List.fold_left2 (fun acc a b -> acc + a + b) 0 third_row forth_row
+  in
+
+  let col_err_pos =
+    calculate_error_bit_pos second_forth_col_sum third_forth_col_sum
+  in
+
+  let row_err_pos =
+    calculate_error_bit_pos second_forth_row_sum third_forth_row_sum
+  in
+  let all_sum = List.fold_left Int.add 0 data in
+
+  match (col_err_pos, row_err_pos, is_even all_sum) with
+  | 1, 1, true -> "No errors or undetectable error"
+  | 1, 1, false -> "No errors in data bits, error in first parity bit"
+  | _, _, true -> "At least two errors"
+  | x, y, false -> Printf.sprintf "One error at %d %d" x y
+
+let random_int_from_os_range max_value =
+  let ic = open_in_bin "/dev/urandom" in
+  let byte = input_byte ic in
+  close_in ic;
+  byte mod max_value
+
+let corrupt_one_bit data =
+  let random_index = random_int_from_os_range 16 in
+  update_list_at data random_index (flip (List.nth data random_index))
+
+let corrupt_two_bit data = corrupt_one_bit data |> corrupt_one_bit
+let corrupt_three_bit data = corrupt_two_bit data |> corrupt_one_bit
+
+let () =
+  let encoded = encode data in
+  print_endline "Encoded data:";
+  pretty_print encoded;
+  print_endline "\n-------------";
+
+  print_endline "\nCorrupitng one bit\n";
+  let corrupted = corrupt_one_bit encoded in
+  let decoded = decode corrupted in
+
+  print_endline "Corrupted data:";
+  pretty_print corrupted;
+
+  print_endline "\nDecoding result:";
+  print_endline decoded;
+
+  print_endline "\n-------------";
+
+  print_endline "\nCorrupitng two bits\n";
+  let corrupted = corrupt_two_bit encoded in
+  let decoded = decode corrupted in
+
+  print_endline "Corrupted data:";
+  pretty_print corrupted;
+
+  print_endline "\nDecoding result:";
+  print_endline decoded;
+
+  print_endline "\n-------------";
+
+  print_endline "\nCorrupitng three bits\n";
+  let corrupted = corrupt_three_bit encoded in
+  let decoded = decode corrupted in
+
+  print_endline "Corrupted data:";
+  pretty_print corrupted;
+
+  print_endline "\nDecoding result:";
+  print_endline decoded
